e269c2b94bb34911aa54654eadda2f83be69712f
rsvictoria-lab <rsvictoria@brc.pshs.edu.ph>
2026-09-09 00:11:53 +0800

    Update classimplementation.py

--- Q1/classimplementation.py
+++ Q1/classimplementation.py
@@ -1,1 +1,12 @@
-
+class FamilyMember:
+  def __init__(self, name: str, role: str, age: int, presence: bool)
+    self.name - name
+    self.role = role 
+    self.__age = age 
+    self.__presence = presence 
+  def get_age(self)
+    return self.__age
+  def is_present(self) 
+    return self.__presence 
+  def set_presence(self)
+    return self.__presence = status 
